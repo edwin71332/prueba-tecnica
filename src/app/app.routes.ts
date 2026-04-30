7783b17a6fb57bf1b1e49d2f93e74dc67260bb28
edwin rodriguez <edwin71332@gmail.com>
2026-04-30 16:00:29 -0400

    add: ruta para el consumo de la api de rick and morty

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { PostListComponent } from './Post-List-Component/post-list-component';
 import { PostFormComponent } from './post-form-component/post-form-component';
+import { richMortyComponent } from './rick-morty-component/rich-morty.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/posts', pathMatch: 'full' },
   { path: 'posts', component: PostListComponent },
   { path: 'posts/new', component: PostFormComponent },
   { path: 'posts/:id', component: PostFormComponent },
- ];
+  { path: 'rick-morty', component: richMortyComponent },
+  { path: 'rick-morty/:id', component: richMortyComponent },
+];
